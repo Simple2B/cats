e2dbded93138de57c47bbccdfe9950e5c26b20cb
Mykola <nchernov@gmail.com>
2023-08-23 18:02:24 +0300

    build only ADM arch

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ VERSION=$(poetry version -s)
 
 # Build docker image for AMD64 and ARM64
 docker buildx build --platform linux/amd64 -t simple2b/cats:latest -t simple2b/cats:${VERSION} --push .
-docker buildx build --platform linux/arm64 -t simple2b/cats:latest -t simple2b/cats:${VERSION} --push .
+# docker buildx build --platform linux/arm64 -t simple2b/cats:latest -t simple2b/cats:${VERSION} --push .
